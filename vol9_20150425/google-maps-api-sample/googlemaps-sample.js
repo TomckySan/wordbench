bc086d725a61f09634ab778312b4eba687d75174
Tomoka Baba <cap11c08931@gmail.com>
2015-04-25 03:13:36 +0900

    complete.

--- vol9_20150425/google-maps-api-sample/googlemaps-sample.js
+++ vol9_20150425/google-maps-api-sample/googlemaps-sample.js
@@ -6,7 +6,8 @@
   $(window).on('load', function() {
     var map,
         currentMarker,
-        targetMarker;
+        targetMarker,
+        directionsRenderer;
 
     var initLat = 0,
         initLng = 0;
@@ -15,7 +16,7 @@
         curLng = 0;
 
     var opt = {
-      zoom: 16,
+      zoom: 10,
       center: new google.maps.LatLng(initLat, initLng),
       mapTypeid: google.maps.MapTypeId.ROADMAP
     };
@@ -49,14 +50,46 @@
     navigator.geolocation.getCurrentPosition(success, error);
 
     $('.input-address').on('change', function() {
-      // TODO
-      // 目的地入力欄。住所を入れるとそこにピンを立ててルート表示
-      // 住所を座標に変換する処理
-      var req = {
-        origin: new google.maps.LatLng(curLat, curLng),
-        destination: targetLatLng,
-        travelMode: google.maps.DirectionsTravelMode.DRIVING
-      };
+      $('.result').text('');
+      var geocoder = new google.maps.Geocoder();
+      geocoder.geocode (
+        {
+          'address':$(this).val(),
+          'region':'jp'
+        },
+        function(result,status) {
+          if(status === google.maps.GeocoderStatus.OK) {
+            var location = result[0].geometry.location;
+            var rendererOpt = {
+              map: map,
+              suppressMarkers: true
+            };
+            if (directionsRenderer) {
+              directionsRenderer.setMap(null);
+            }
+            directionsRenderer = new google.maps.DirectionsRenderer();
+            directionsRenderer.setMap(map);
+            var directionsService = new google.maps.DirectionsService();
+            var req = {
+              origin: new google.maps.LatLng(curLat, curLng),
+              destination: new google.maps.LatLng(location.k, location.D),
+              travelMode: google.maps.DirectionsTravelMode.DRIVING
+            };
+            directionsService.route(req, function(result,status){
+              if (status === google.maps.DirectionsStatus.OK) {
+                directionsRenderer.setDirections(result);
+                var legs = result.routes[0].legs;
+                var journey = 0;
+                for (var i in legs) {
+                  journey += legs[i].distance.value;
+                }
+                var distance = journey / 1000;
+                $('.result').text('現在地から'+$('.input-address').val()+'までの距離は'+distance+'kmらしいよ');
+              }
+            });
+          }
+        }
+      );
     });
   });
 }).call(this);
