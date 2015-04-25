9574deeeab3dfad2469825b5c3b9fc7e78fa89cb
Tomoka Baba <cap11c08931@gmail.com>
2015-04-25 13:34:40 +0900

    complete.

--- vol9_20150425/geolocation-api-sample/geolocation-sample.js
+++ vol9_20150425/geolocation-api-sample/geolocation-sample.js
@@ -9,12 +9,7 @@
   var success = function(pos) {
     console.log(pos.coords);
     var info = '緯度:' + pos.coords.latitude + '<br/>' +
-               '軽度:' + pos.coords.longitude + '<br/>' +
-               '高度:' + pos.coords.altitude + '<br/>' +
-               '緯度・経度の誤差:' + pos.coords.accuracy + '<br/>' +
-               '高度の誤差:' + pos.coords.altitudeAccuracy + '<br/>' +
-               '方角:' + pos.coords.heading + '<br/>' +
-               '速度:' + pos.coords.speed + '<br/>';
+               '経度:' + pos.coords.longitude + '<br/>';
     console.log(info);
     document.getElementById('current_info').innerHTML = info;
   };
